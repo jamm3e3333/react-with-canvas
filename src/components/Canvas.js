e1a1b9ef809e758bd1a6f6d27c725d3abc8c424b
JakubVala <jamm3e3333@gmail.com>
2021-08-12 22:08:41 +0200

    image added

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect, useCallback } from 'react';
-
+import canvasBg from '../assets/images/map__view_title.jpg'
 import classes from './Canvas.module.css';   
-
+const img1 = new Image();
+img1.src = canvasBg;
 const Canvas = props => {
 
     const canvasRef = useRef();
@@ -9,7 +10,7 @@ const Canvas = props => {
     const draw = useCallback(ctx => {
         ctx.fillStyle = '#FF0000'
         ctx.beginPath()
-        ctx.arc(50, 100, 8, 0, 2*Math.PI)
+        ctx.arc(50, 100, 6, 0, 2*Math.PI)
         ctx.fill()
       }, []);
 
@@ -20,8 +21,7 @@ const Canvas = props => {
         canvas.width = 640;
         canvas.height = 400;
         const ctx = canvas.getContext('2d');
-        ctx.fillStyle = ('#00FF00');
-        ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
+        ctx.drawImage(img1, 0 , 0)
     
         draw(ctx);
     }, [draw]);
