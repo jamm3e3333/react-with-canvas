d6765a523bcc2aaf9f1de8611ce86ae9b97c09b0
Jam3e3333 <jamm3e3333@gmail.com>
2021-08-13 10:02:32 -0400

    drawing coordinations

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,16 +1,219 @@
-import React, { useRef, useEffect, useCallback } from 'react';
+import React, { useRef, useEffect, useCallback, useMemo } from 'react';
 import canvasBg from '../assets/images/map__view_title.jpg'
-import classes from './Canvas.module.css';   
-const img1 = new Image();
-img1.src = canvasBg;
+import classes from './Canvas.module.css';  
+
+
+const coords = {
+    //first 3 places
+    0: {
+      coord: {
+        x: 90,
+        y: 140
+      }
+    },
+    1: {
+      coord: {
+        x: 110,
+        y: 160
+      }
+    },
+    2: {
+      coord: {
+        x: 130,
+        y: 180
+      }
+    },
+  
+    //first lane
+    3: {
+      coord: {
+        x: 170,
+        y: 200
+      }
+    },
+    4: {
+      coord: {
+        x: 200,
+        y: 180
+      }
+    },
+    5: {
+      coord: {
+        x: 230,
+        y: 160
+      }
+    },
+    6: {
+      coord: {
+        x: 260,
+        y: 140
+      }
+    },
+    7: {
+      coord: {
+        x: 290,
+        y: 120
+      }
+    },
+    8: {
+      coord: {
+        x: 320,
+        y: 100
+      }
+    },
+    9: {
+      coord: {
+        x: 350,
+        y: 75
+      }
+    },
+    10: {
+      coord: {
+        x: 380,
+        y: 50
+      }
+    },
+    11: {
+      coord: {
+        x: 410,
+        y: 25
+      }
+    },
+  
+    12: {
+      coord: {
+        x: 290,
+        y: 230
+      }
+    },
+    13: {
+      coord: {
+        x: 310,
+        y: 215
+      }
+    },
+    14: {
+      coord: {
+        x: 330,
+        y: 200
+      }
+    },
+    15: {
+      coord: {
+        x: 350,
+        y: 185
+      }
+    },
+    16: {
+      coord: {
+        x: 370,
+        y: 170
+      }
+    },
+    17: {
+      coord: {
+        x: 390,
+        y: 155
+      }
+    },
+    18: {
+      coord: {
+        x: 410,
+        y: 140
+      }
+    },
+    19: {
+      coord: {
+        x: 430,
+        y: 125
+      }
+    },
+    20: {
+      coord: {
+        x: 450,
+        y: 110
+      }
+    },
+    21: {
+      coord: {
+        x: 470,
+        y: 95
+      }
+    },
+  
+  
+    22: {
+      coord: {
+        x: 310,
+        y: 250
+      }
+    },
+    23: {
+      coord: {
+        x: 330,
+        y: 235
+      }
+    },
+    24: {
+      coord: {
+        x: 350,
+        y: 220
+      }
+    },
+    25: {
+      coord: {
+        x: 370,
+        y: 205
+      }
+    },
+    26: {
+      coord: {
+        x: 390,
+        y: 190
+      }
+    },27: {
+      coord: {
+        x: 410,
+        y: 175
+      }
+    },
+    28: {
+      coord: {
+        x: 430,
+        y: 160
+      }
+    },
+    29: {
+      coord: {
+        x: 450,
+        y: 145
+      }
+    },
+    30: {
+      coord: {
+        x: 470,
+        y: 130
+      }
+    },
+    31: {
+      coord: {
+        x: 490,
+        y: 115
+      }
+    }
+  };
+
 const Canvas = props => {
+    const img = useMemo(() => new Image(), []);
+    
+    img.src = canvasBg;
 
     const canvasRef = useRef();
 
-    const draw = useCallback(ctx => {
+    const draw = useCallback((ctx,x,y) => {
         ctx.fillStyle = '#FF0000'
         ctx.beginPath()
-        ctx.arc(50, 100, 6, 0, 2*Math.PI)
+        ctx.arc(x, y, 6, 0, 2*Math.PI)
         ctx.fill()
       }, []);
 
@@ -21,10 +224,14 @@ const Canvas = props => {
         canvas.width = 640;
         canvas.height = 400;
         const ctx = canvas.getContext('2d');
-        ctx.drawImage(img1, 0 , 0)
-    
-        draw(ctx);
-    }, [draw]);
+        img.onload = () => {
+            ctx.drawImage(img, 0 , 0); 
+            for(let i = 0; i < 32; i++) {
+                draw(ctx, coords[i].coord.x, coords[i].coord.y);
+            }
+        }
+
+    }, [draw, img]);
 
 
 
